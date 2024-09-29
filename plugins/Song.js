1dd40ac5cc398c01ca8b331edf1e1d7a336facd4
DRKALPHA <146550095+DRKALPHA@users.noreply.github.com>
2024-09-29 06:47:12 +0530

    Update Song.js

--- plugins/Song.js
+++ plugins/Song.js
@@ -1,1 +1,30 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
 
+
+cmd({
+    pattern: "song",
+    desc: "download songs",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please give me url or title")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+
+let desc = `
+☛ ALPHABOT SONG DOWNLOADER ☚
+
+title: ${data.title}
+description: ${data.description}
+`
+
+
+}catch(e){
+console.log(e)
+reply("${e}")
+}
