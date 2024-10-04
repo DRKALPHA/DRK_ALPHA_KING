ed936fa9ecbf4605df33ce753c4790bc107c2925
DRKALPHA <146550095+DRKALPHA@users.noreply.github.com>
2024-10-04 06:44:49 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -26,6 +26,7 @@ menu[commands[i].category] += `.${commands[i].pattern}\n`;
      }
 
 let madeMenu = `*Hello ${pushname}*
+
 > *DOWNLOAD COMMANDS* ⬇️
 
 ${menu.download}
@@ -34,16 +35,28 @@ ${menu.download}
 
 ${menu.main}
 
- > *SEARCH COMMANDS* 
- ${menu.search}
+> *SEARCH COMMANDS* 📡
 
+${menu.search}
 
+> *GROUP COMMANDS* ✵
 
+${menu.group}
 
+> *OWNER COMMANDS* 🧑‍🔧
 
+${menu.owner}
+
+> *CONVERT COMMANDS*
+
+${menu.convert}
+
+⚔ POWERD BY DRK DEVELOPER ⚔
 `
-
+await conn.sendMessage(form,{image:{url:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.facebook.com%2FAlphaKingYT%2F&psig=AOvVaw1hIRn3iahHaOP3F49jFzq0&ust=1728090467968000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKj5pvXE84gDFQAAAAAdAAAAABAE"},caption:madeMenu},{quoted:mek})
 
 }catch(e){
 console.log(e)
 reply('${e}')
+  }
+})
