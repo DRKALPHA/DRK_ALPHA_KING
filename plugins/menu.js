e4a4cd7e0287269340805b604c4f233f6fa17064
DRKALPHA <146550095+DRKALPHA@users.noreply.github.com>
2024-10-04 08:06:35 +0530

    Update and rename menu-2.js to menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -37,59 +37,59 @@ menu[commands[i].category] += `.${commands[i].pattern}\n`;
 
 let madeMenu = ` *👋 Hello ${pushname}*
 
-✨ 𝗪𝗲𝗹𝗰𝗼𝗺𝗲 𝘁𝗼 𝗡𝗘𝗧𝗛𝗨-𝗠𝗗! ✨ 
+✨ 𝗪𝗲𝗹𝗰𝗼𝗺𝗲 𝘁𝗼 𝗗𝗔𝗞-𝗠𝗗! ✨ 
 ╭─「 ᴄᴏᴍᴍᴀɴᴅꜱ ᴘᴀɴᴇʟ 」
 │◈ ʀᴜɴᴛɪᴍᴇ : ${runtime(process.uptime())}
-│◈ ᴏᴡɴᴇʀ ɴᴀᴍᴇ : ɴᴇᴛʜᴍɪᴋᴀ ᴍᴀɪɴ
-│◈ ᴏᴡɴᴇʀ ɴᴜᴍʙᴇʀ : 94704227534
+│◈ ᴏᴡɴᴇʀ ɴᴀᴍᴇ : ᴅʀᴋ ᴅᴇᴠᴇʟᴏᴘᴇʀ
+│◈ ᴏᴡɴᴇʀ ɴᴜᴍʙᴇʀ : 94763906841
 ╰──────────●●►
 ╭──────────●●►
- 📥 *𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐌𝐞𝐧𝐮*
+ 📥 *𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐌𝐞𝐧𝐮* 📥
   ───────
  ${menu.download}
 ╰───────────●●►
 ╭──────────●●►
- 🔧 *𝐌𝐚𝐢𝐧 𝐌𝐞𝐧𝐮*
+ 🔧 *𝐌𝐚𝐢𝐧 𝐌𝐞𝐧𝐮* 🔧
   ───────
  ${menu.main}
 ╰───────────●●►
 ╭──────────●●►
- 🎉 *𝐅𝐮𝐧 𝐌𝐞𝐧𝐮*
+ 🎉 *𝐅𝐮𝐧 𝐌𝐞𝐧𝐮* 🎉
   ───────
  ${menu.fun}
 ╰───────────●●►
 ╭──────────●●►
- 🔄 *𝐂𝐨𝐧𝐯𝐞𝐫𝐭 𝐌𝐞𝐧𝐮*
+ 🔄 *𝐂𝐨𝐧𝐯𝐞𝐫𝐭 𝐌𝐞𝐧𝐮* 🔄
   ───────
  ${menu.convert}
 ╰───────────●●►
 ╭──────────●●►
- 🔍 *𝐒𝐞𝐚𝐫𝐜𝐡 𝐌𝐞𝐧𝐮*
+ 🔍 *𝐒𝐞𝐚𝐫𝐜𝐡 𝐌𝐞𝐧𝐮* 🔍
   ───────
  ${menu.search}
 ╰───────────●●►
 ╭──────────●●►
- 👥 *𝐆𝐫𝐨𝐮𝐩 𝐌𝐞𝐧𝐮*
+ 👥 *𝐆𝐫𝐨𝐮𝐩 𝐌𝐞𝐧𝐮* 👥
   ───────
  ${menu.group}
 ╰───────────●●►
 ╭──────────●●►
- 🔒 *𝐎𝐰𝐧𝐞𝐫 𝐌𝐞𝐧𝐮*
+ 🔒 *𝐎𝐰𝐧𝐞𝐫 𝐌𝐞𝐧𝐮* 🔒
   ───────
  ${menu.owner}
 ╰───────────●●►
 ╭──────────●●►
- ⚙️ *𝐎𝐭𝐡𝐞𝐫 𝐌𝐞𝐧𝐮*
+ ⚙️ *𝐎𝐭𝐡𝐞𝐫 𝐌𝐞𝐧𝐮* ⚙️
   ───────
  ${menu.other}
 ╰───────────●●►
 
-> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴍɪᴋᴀ ᴍᴀɪɴ*
+> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅʀᴋ ᴅᴇᴠᴇʟᴏᴘᴇʀ ᴍᴀɪɴ*
 `
 
 await conn.sendMessage(from, { audio: { url: voice.menu }, mimetype: 'audio/mp4', ptt: true }, { quoted: mek })
 
-return await conn.sendMessage(from,{image: {url: `https://imgtr.ee/images/2024/09/17/37f9be869d1404ef1a5d188178450537.jpeg`},caption:madeMenu},{quoted: mek})
+return await conn.sendMessage(from,{image: {url: `https://www.imgtr.net/LnJsCASUkzK6DmA`},caption:madeMenu},{quoted: mek})
 }catch(e){
 console.log(e)
 reply(`Error`)
